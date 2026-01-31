5abd17a1080618e6d5c8883f607bb34a12699543
Brendan Lahey <lahey1991@gmail.com>
2026-01-31 17:38:32 -0600

    WIP:base  repo

---
+++ src/persistance/infrastructure/sqlalchemy/data_repository.py
@@ -0,0 +1,7 @@
+from sqlalchemy import select
+from src.persistance.domain.data_repository import DataRepository
+
+class SqlAlchemyDataRepositoy(DataRepository):
+    def __init__(self, model):
+        pass
+
