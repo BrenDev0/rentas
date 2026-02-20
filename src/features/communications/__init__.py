2fd1f9c1533550e899cb6d4216d46dabede59b17
Brendan Lahey <lahey1991@gmail.com>
2026-02-20 17:42:04 -0600

    init desc

---
+++ src/features/communications/__init__.py
@@ -0,0 +1,13 @@
+"""
+Structure:
+- Domain: Abstract interfaces, schemas, entities and models
+- Application: Domain applications (use cases, rules)
+- Infrastructure: Framework implementations 
+- Interface: accesspoint
+- Di: registry
+"""
+
+__version__ = "1.0.0"
+__author__ = "BrenDev0"
+__description__ = "Communications package for app"
+
